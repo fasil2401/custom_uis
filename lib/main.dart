f03aaf15fe87dd92bba0b00c8a570dec4df05463
fasil2401 <fasil2401@gmail.com>
2022-02-20 15:05:04 +0530

    final last with navigation

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(       
         primarySwatch: Colors.blue,
       ),
-      home:  Sample2(),
+      home:  AdditionalInformation(),
       debugShowCheckedModeBanner: false,
     );
   }
